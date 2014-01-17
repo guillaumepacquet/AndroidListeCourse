ea3e0eef881c1be92283468ad9e85b203184689f
guillaume <guillaume@192.168.21.67>
2014-01-17 14:22:07 +0100

    passage de la splashactivity a la main activity avec le bouton demarrer

--- src/com/esaip/android/marketList/SplashActivity.java
+++ src/com/esaip/android/marketList/SplashActivity.java
@@ -1,15 +1,29 @@
 package com.esaip.android.marketList;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class SplashActivity extends Activity {
-
+	public static final String TAG = SplashActivity.class.getName();
+	public View btnStart;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		btnStart= findViewById(R.id.btnStart);
+		btnStart.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(SplashActivity.this,MainActivity.class));
+			}
+		});
 	}
 
 	@Override
